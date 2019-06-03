5cceb4e4b29cced8de25e2d3a5fa0078f926850d
alexalok <work@alexalok.me>
2019-06-03 11:26:35 +0300

    Initial commit

---
+++ ConsoleExt/ConsoleExt.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ConsoleExtensions
+{
+    public static class ConsoleExt
+    {
+        public static void WriteLines(params string[] lines)
+        {
+            foreach (var line in lines)
+            {
+                Console.WriteLine(line);
+            }
+        }
+
+        public static void WriteLineWarn(string line)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine(line);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public static void WriteLineError(string line)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(line);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public static void WriteHr()
+        {
+            var width = Console.WindowWidth;
+            for (var i = 0; i < width; i++)
+                Console.Write("-");
+            Console.WriteLine();
+        }
+
+        public static void SkipScreen()
+        {
+            var consoleHeight = Console.WindowHeight;
+            for (var i = 0; i < consoleHeight; i++)
+                Console.WriteLine();
+        }
+
+        public static void Pause()
+        {
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ReadLineExtensions/ReadLineExt.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ReadLineExtensions
+{
+    public static class ReadLineExt
+    {
+        public static int? TryReadAsInt(string prompt = "", string @default = "")
+        {
+            var input = ReadLine.Read(prompt, @default);
+            return int.TryParse(input, out var result) ? result : (int?)null;
+        }
+
+        public static bool ReadAsBool(string prompt = "(y/N)> ", string @default = "N")
+        {
+            var input = ReadLine.Read(prompt, @default);
+            return input.Length > 0 && input.Trim().Substring(0, 1).ToLowerInvariant() == "y";
+        }
+    }
+}
